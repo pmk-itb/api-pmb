fc64088d2e63918e0f62f9c50fd856b34781b15d
stefanusgusega <gunawanstefanus006@gmail.com>
2021-05-16 23:28:30 +0700

    chore: add prisma and prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT;
 
 app.get('/', (_req, res) => {
