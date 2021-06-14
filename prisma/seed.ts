014ef6fac97d1ccd8addc6f81e1d90d5ea19b789
stefanusgusega <gunawanstefanus006@gmail.com>
2021-06-14 15:14:53 +0700

    feat(orm): add seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+const deptData = [
+  {
+    code: 165,
+    name: 'Sekolah Teknik Elektro dan Informatika',
+    majors: {
+      create: {
+        // nama nama jurusan
+      },
+    },
+  },
+  {
+    // fakultas lain
+  },
+];
