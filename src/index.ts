63aacf0a49165937d397ffda93cb4e6125743b68
stefanusgusega <gunawanstefanus006@gmail.com>
2021-06-19 18:28:22 +0700

    refactor: remove meaningless comments

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import api from './api-routes';
-// import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
-// const prisma = new PrismaClient();
 const port = process.env.PORT;
 
 app.use('/api', api);
