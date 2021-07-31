4e927326f0546b9c942a1c75654f44095953d52b
stefanusgusega <gunawanstefanus006@gmail.com>
2021-07-31 16:27:50 +0700

    test: all remaining app.ts

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  setupFilesAfterEnv: ['./testing-setup.ts'],
 };
--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,8 +1,31 @@
+import { Location } from '@prisma/client';
 import request from 'supertest';
 import app from '../app';
 
 describe('Test the url', () => {
-  test('It should response the GET method', () => {
+  it('should response 200 when hit /api', () => {
+    jest.setTimeout(10000);
     return request(app).get('/api').expect(200);
+  });
+
+  it('should response 200 when hit /api/about', () => {
+    return request(app).get('/api/about').expect(200);
+  });
+
+  it('should response 200 when hit /api/departments', () => {
+    return request(app).get('/api/departments').expect(200);
+  });
+
+  it('should be able to create department from /api/departments', () => {
+    const data = {
+      code: 'STEI',
+      name: 'Sekolah Teknik dan Informatika',
+      location: Location.GANESA,
+    };
+    return request(app).post('/api/departments').send(data).expect(200);
+  });
+
+  it('should response 200 when hit /api/test', () => {
+    return request(app).get('/api/test').expect(200);
   });
 });
