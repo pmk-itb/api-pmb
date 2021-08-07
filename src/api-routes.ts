29cc7fde2bbf269dd3815bd10ef6c1ca1e4ca2ab
Rexy-Gamaliel <rexygaming400@gmail.com>
2021-08-07 18:15:23 +0700

    feat: added create member endpoint

--- src/api-routes.ts
+++ src/api-routes.ts
@@ -65,4 +65,110 @@ router.get('/test', async (_req: Request, res: Response) => {
   res.json(variable);
 });
 
+// Endpoint to create member
+router.post('/members', async (req, res) => {
+  /** Format penamaan elemen di html snake case or camel case? */
+  const {
+    nim,
+    name,
+    nickname,
+    majorId,
+    gender,
+    birthDate,
+    year,
+    line,
+    phone,
+    email,
+    currentAddress,
+    originProvince,
+    originAddress,
+    originSchool,
+    currentChurch,
+    originChurch,
+    status,
+    notes,
+    parentName,
+    parentPhone,
+    parentRelationship,
+    discipleshipId,
+  } = req.body;
+
+  const createParent = await prisma.parent.create({
+    data: {
+      name: parentName,
+      phone: parentPhone,
+      relationship: parentRelationship,
+    },
+  });
+
+  const upsertDiscipleship = await prisma.discipleship.upsert({
+    create: {
+      status: 'PENDING',
+    },
+    update: {},
+    where: {
+      id: discipleshipId,
+    },
+  });
+
+  const member = await prisma.member.create({
+    data: {
+      nim,
+      tpbNim: nim,
+      name,
+      nickname,
+      major: { connect: { id: majorId } },
+      gender,
+      birthDate: birthDate,
+      year,
+      line,
+      phone,
+      email,
+      currentAddress: currentAddress,
+      originProvince: originProvince,
+      originAddress: originAddress,
+      originSchool: originSchool,
+      currentChurch: currentChurch,
+      originChurch: originChurch,
+      status,
+      notes,
+      parent: { connect: { id: createParent.id } },
+      discipleship: { connect: { id: discipleshipId } },
+    },
+  });
+
+  res.json({
+    createParent,
+    upsertDiscipleship,
+    member,
+  });
+});
+/**
+Test case create Member (JSON):
+{
+  "nim":13519001,
+  "name":"Joko Santoso",
+  "nickname":"Joko",
+  "majorId":73,
+  "gender":"MALE",
+  "birthDate":"2000-01-01T00:00:00Z",
+  "year":2019,
+  "line":"jk",
+  "phone":"021500500",
+  "email":"a@gmail.com",
+  "currentAddress":"Jl. A No. 3",
+  "originProvince":"Jakarta",
+  "originAddress":"Jl.B No. 15",
+  "originSchool":"SMAN 1 Jakarta",
+  "currentChurch":"HKBP B",
+  "originChurch":"HKBP A",
+  "status":"ACTIVE",
+  "notes":"-",
+  "parentName":"Test Parent",
+  "parentPhone":"08111111111",
+  "parentRelationship":"AYAH",
+  "discipleshipId":1
+}
+*/
+
 export { router, prisma };
