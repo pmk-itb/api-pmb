349e51104aea65cb18ae36cab5bfbfe741419be6
adriantimotheus <timotheusadrian@gmail.com>
2021-08-10 04:30:28 +0700

    added formatGereja and formatSekolah function

--- src/sanitization.ts
+++ src/sanitization.ts
@@ -1,6 +1,6 @@
 // import { format } from "path/posix"
 
-let formatName = (inputName) =>{
+export let formatName = (inputName) =>{
     let names = inputName.split(" ");
     console.log(names);
     let formattedName = "";
@@ -11,7 +11,7 @@ let formatName = (inputName) =>{
     return formattedName;
 }
 
-let formatTelp = (no_telp) => {
+export let formatTelp = (no_telp) => {
     let telp = "";
     if(String(no_telp).includes("+")){
         //replace +62 with 0
@@ -21,4 +21,44 @@ let formatTelp = (no_telp) => {
         telp = no_telp;
     }
     return(telp);
+}
+
+export let formatSekolah = (sekolah) => {
+    let nama_sekolah = sekolah.split(" ");
+    let formatted_nama_sekolah = ""
+
+    formatted_nama_sekolah += nama_sekolah[0].toUpperCase()
+    nama_sekolah.shift()
+
+    for (const word of nama_sekolah){
+        const temp = word.charAt(0).toUpperCase() + word.slice(1)
+        console.log(temp)
+        switch(temp){
+            case("Kristen"):
+                formatted_nama_sekolah += "K";
+                break;
+            case("Negeri"):
+                formatted_nama_sekolah += "N";
+                break;
+            case("Swasta"):
+                formatted_nama_sekolah += "S";
+                break;
+            default:
+                formatted_nama_sekolah += " "+ temp
+        }
+    }
+    return formatted_nama_sekolah;
+}
+
+export let formatGereja = (gereja) => {
+    let nama_gereja = gereja.split(" ");
+    let formatted_nama_gereja = ""
+
+    formatted_nama_gereja += nama_gereja[0].toUpperCase()
+    nama_gereja.shift()
+
+    for (const word of nama_gereja){
+        formatted_nama_gereja +=  " " + word.charAt(0).toUpperCase() + word.slice(1)
+    }
+    return formatted_nama_gereja;
 }
