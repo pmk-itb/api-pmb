98764efa217e6bfa06661ff594aca405c42c1a7e
adriantimotheus <timotheusadrian@gmail.com>
2021-08-22 20:25:45 +0700

    feat: added sanitization function for nickname and parentPhone

--- src/middlewares/sanitization.middleware.ts
+++ src/middlewares/sanitization.middleware.ts
@@ -3,7 +3,9 @@ import * as sanitization from '../sanitization';
 
 export const sanitizeData = (req: Request, _res: Response, next: NextFunction): void => {
   req.body.name = sanitization.formatName(req.body.name);
+  req.body.nickname = sanitization.formatName(req.body.nickname);
   req.body.phone = sanitization.formatTelp(req.body.phone);
+  req.body.parentPhone = sanitization.formatTelp(req.body.parentPhone);
   req.body.originSchool = sanitization.formatSekolah(req.body.originSchool);
   req.body.originChurch = sanitization.formatGereja(req.body.originChurch);
   return next();
