e48200440d679a7b667bd091b9bef3d06c4d30a0
stefanusgusega <gunawanstefanus006@gmail.com>
2021-08-25 23:38:50 +0700

    test: add load test script for get req

--- src/__tests__/loads/locustfile.py
+++ src/__tests__/loads/locustfile.py
@@ -1,7 +1,20 @@
-from locust import HttpUser, task
+from dotenv import dotenv_values
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
+
+class FormPMBUser(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        api_key = dotenv_values('../../../.env.development')['API_KEY']
+        self.client.headers = {
+            'Authorization': 'Bearer ' + api_key
+        }
+
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def get_departments(self):
+        self.client.get('/departments')
+
+    @task
+    def get_mentors(self):
+        self.client.get('/mentors')
