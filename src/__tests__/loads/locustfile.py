a3adabcde8a15a006446ceb268336141e82a4cb2
stefanusgusega <gunawanstefanus006@gmail.com>
2021-08-28 01:26:06 +0700

    test: add post load test

--- src/__tests__/loads/locustfile.py
+++ src/__tests__/loads/locustfile.py
@@ -1,9 +1,10 @@
 from dotenv import dotenv_values
 from locust import HttpUser, task, between
+from faker import Faker
 
 
 class FormPMBUser(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(20, 30)
 
     def on_start(self):
         api_key = dotenv_values('../../../.env.development')['API_KEY']
@@ -12,9 +13,29 @@ class FormPMBUser(HttpUser):
         }
 
     @task
-    def get_departments(self):
+    def get_departments_and_mentors(self):
         self.client.get('/departments')
+        self.client.get('/mentors')
 
     @task
-    def get_mentors(self):
-        self.client.get('/mentors')
+    def create_member(self):
+        faker = Faker(['id_ID'])
+        data = {
+            'nim': faker.random.randint(16521001, 16521600),
+            'name': faker.name(),
+            'nickname': faker.first_name(),
+            'majorId': faker.random.randint(1, 77),
+            'gender': 'MALE',
+            'birthDate': '2000-09-02T00:00:00Z',
+            'line': faker.user_name(),
+            'phone': faker.phone_number(),
+            'email': faker.email(),
+            'originProvince': faker.state(),
+            'originCity': faker.city(),
+            'originSchool': faker.company(),
+            'originChurch': faker.company(),
+            'parentPhone': faker.phone_number(),
+            'parentRelationship': 'AYAH',
+            'discipleshipId': faker.random.randint(1, 85)
+        }
+        self.client.post(url='/members', json=data)
