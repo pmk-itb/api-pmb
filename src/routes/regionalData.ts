cfc9092ede37964f328d96f8692f647c5832ab34
Samuel <samnap11@gmail.com>
2021-08-29 21:45:49 +0700

    feat: add route for school list

--- src/routes/regionalData.ts
+++ src/routes/regionalData.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { listProvinces } from '../controller/province';
 import { getCitiesByProvince } from '../controller/kabupaten';
+import { getSchools } from '../controller/school';
 
 const router = Router();
 
 router.get('/provinces', listProvinces);
 
 router.get('/cities', getCitiesByProvince);
+
+router.get('/schools', getSchools);
 
 export default router;
