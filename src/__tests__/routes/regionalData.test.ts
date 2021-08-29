941b5a80c9927256b3bb245ddafdfdd19a7d6d68
Samuel <samnap11@gmail.com>
2021-08-29 21:46:14 +0700

    test: add new test for school route

--- src/__tests__/routes/regionalData.test.ts
+++ src/__tests__/routes/regionalData.test.ts
@@ -3,44 +3,45 @@ import app from '../../app';
 
 describe('Test Regional Data Routes', () => {
   it('should return list of provinces', async () => {
-    const { body, status } = await request(app).get('/api/provinces');
+    const token = process.env.API_KEY as string;
+    const { body, status } = await request(app).get('/api/provinces').auth(token, { type: 'bearer' });
 
     const expected = {
       data: [
-        { id: '11', nama: 'ACEH' },
-        { id: '12', nama: 'SUMATERA UTARA' },
-        { id: '13', nama: 'SUMATERA BARAT' },
-        { id: '14', nama: 'RIAU' },
-        { id: '15', nama: 'JAMBI' },
-        { id: '16', nama: 'SUMATERA SELATAN' },
-        { id: '17', nama: 'BENGKULU' },
-        { id: '18', nama: 'LAMPUNG' },
-        { id: '19', nama: 'KEPULAUAN BANGKA BELITUNG' },
-        { id: '21', nama: 'KEPULAUAN RIAU' },
-        { id: '31', nama: 'DKI JAKARTA' },
-        { id: '32', nama: 'JAWA BARAT' },
-        { id: '33', nama: 'JAWA TENGAH' },
-        { id: '34', nama: 'DAERAH ISTIMEWA YOGYAKARTA' },
-        { id: '35', nama: 'JAWA TIMUR' },
-        { id: '36', nama: 'BANTEN' },
-        { id: '51', nama: 'BALI' },
-        { id: '52', nama: 'NUSA TENGGARA BARAT' },
-        { id: '53', nama: 'NUSA TENGGARA TIMUR' },
-        { id: '61', nama: 'KALIMANTAN BARAT' },
-        { id: '62', nama: 'KALIMANTAN TENGAH' },
-        { id: '63', nama: 'KALIMANTAN SELATAN' },
-        { id: '64', nama: 'KALIMANTAN TIMUR' },
-        { id: '65', nama: 'KALIMANTAN UTARA' },
-        { id: '71', nama: 'SULAWESI UTARA' },
-        { id: '72', nama: 'SULAWESI TENGAH' },
-        { id: '73', nama: 'SULAWESI SELATAN' },
-        { id: '74', nama: 'SULAWESI TENGGARA' },
-        { id: '75', nama: 'GORONTALO' },
-        { id: '76', nama: 'SULAWESI BARAT' },
-        { id: '81', nama: 'MALUKU' },
-        { id: '82', nama: 'MALUKU UTARA' },
-        { id: '91', nama: 'P A P U A' },
-        { id: '92', nama: 'PAPUA BARAT' },
+        { id: '06', nama: 'ACEH' },
+        { id: '07', nama: 'SUMATERA UTARA' },
+        { id: '08', nama: 'SUMATERA BARAT' },
+        { id: '09', nama: 'RIAU' },
+        { id: '10', nama: 'JAMBI' },
+        { id: '11', nama: 'SUMATERA SELATAN' },
+        { id: '26', nama: 'BENGKULU' },
+        { id: '12', nama: 'LAMPUNG' },
+        { id: '29', nama: 'KEPULAUAN BANGKA BELITUNG' },
+        { id: '31', nama: 'KEPULAUAN RIAU' },
+        { id: '01', nama: 'DKI JAKARTA' },
+        { id: '02', nama: 'JAWA BARAT' },
+        { id: '03', nama: 'JAWA TENGAH' },
+        { id: '04', nama: 'DAERAH ISTIMEWA YOGYAKARTA' },
+        { id: '05', nama: 'JAWA TIMUR' },
+        { id: '28', nama: 'BANTEN' },
+        { id: '22', nama: 'BALI' },
+        { id: '23', nama: 'NUSA TENGGARA BARAT' },
+        { id: '24', nama: 'NUSA TENGGARA TIMUR' },
+        { id: '13', nama: 'KALIMANTAN BARAT' },
+        { id: '14', nama: 'KALIMANTAN TENGAH' },
+        { id: '15', nama: 'KALIMANTAN SELATAN' },
+        { id: '16', nama: 'KALIMANTAN TIMUR' },
+        { id: '34', nama: 'KALIMANTAN UTARA' },
+        { id: '17', nama: 'SULAWESI UTARA' },
+        { id: '18', nama: 'SULAWESI TENGAH' },
+        { id: '19', nama: 'SULAWESI SELATAN' },
+        { id: '20', nama: 'SULAWESI TENGGARA' },
+        { id: '30', nama: 'GORONTALO' },
+        { id: '33', nama: 'SULAWESI BARAT' },
+        { id: '21', nama: 'MALUKU' },
+        { id: '27', nama: 'MALUKU UTARA' },
+        { id: '25', nama: 'PAPUA' },
+        { id: '32', nama: 'PAPUA BARAT' },
       ],
     };
 
@@ -49,33 +50,34 @@ describe('Test Regional Data Routes', () => {
   });
 
   it('should return kabupaten from provinsi Aceh', async () => {
-    const { body, status } = await request(app).get('/api/cities?province_id=11');
+    const token = process.env.API_KEY as string;
+    const { body, status } = await request(app).get('/api/cities?province_id=06').auth(token, { type: 'bearer' });
 
     const expected = {
       data: [
-        { id: '1101', nama: 'KAB. ACEH SELATAN' },
-        { id: '1102', nama: 'KAB. ACEH TENGGARA' },
-        { id: '1103', nama: 'KAB. ACEH TIMUR' },
-        { id: '1104', nama: 'KAB. ACEH TENGAH' },
-        { id: '1105', nama: 'KAB. ACEH BARAT' },
-        { id: '1106', nama: 'KAB. ACEH BESAR' },
-        { id: '1107', nama: 'KAB. PIDIE' },
-        { id: '1108', nama: 'KAB. ACEH UTARA' },
-        { id: '1109', nama: 'KAB. SIMEULUE' },
-        { id: '1110', nama: 'KAB. ACEH SINGKIL' },
-        { id: '1111', nama: 'KAB. BIREUEN' },
-        { id: '1112', nama: 'KAB. ACEH BARAT DAYA' },
-        { id: '1113', nama: 'KAB. GAYO LUES' },
-        { id: '1114', nama: 'KAB. ACEH JAYA' },
-        { id: '1115', nama: 'KAB. NAGAN RAYA' },
-        { id: '1116', nama: 'KAB. ACEH TAMIANG' },
-        { id: '1117', nama: 'KAB. BENER MERIAH' },
-        { id: '1118', nama: 'KAB. PIDIE JAYA' },
-        { id: '1171', nama: 'KOTA BANDA ACEH' },
-        { id: '1172', nama: 'KOTA SABANG' },
-        { id: '1173', nama: 'KOTA LHOKSEUMAWE' },
-        { id: '1174', nama: 'KOTA LANGSA' },
-        { id: '1175', nama: 'KOTA SUBULUSSALAM' },
+        { id: '060700', nama: 'KAB. ACEH SELATAN' },
+        { id: '060800', nama: 'KAB. ACEH TENGGARA' },
+        { id: '060400', nama: 'KAB. ACEH TIMUR' },
+        { id: '060500', nama: 'KAB. ACEH TENGAH' },
+        { id: '060600', nama: 'KAB. ACEH BARAT' },
+        { id: '060100', nama: 'KAB. ACEH BESAR' },
+        { id: '060200', nama: 'KAB. PIDIE' },
+        { id: '060300', nama: 'KAB. ACEH UTARA' },
+        { id: '061100', nama: 'KAB. SIMEULUE' },
+        { id: '061300', nama: 'KAB. ACEH SINGKIL' },
+        { id: '061200', nama: 'KAB. BIREUEN' },
+        { id: '061700', nama: 'KAB. ACEH BARAT DAYA' },
+        { id: '061800', nama: 'KAB. GAYO LUES' },
+        { id: '061600', nama: 'KAB. ACEH JAYA' },
+        { id: '061500', nama: 'KAB. NAGAN RAYA' },
+        { id: '061400', nama: 'KAB. ACEH TAMIANG' },
+        { id: '061900', nama: 'KAB. BENER MERIAH' },
+        { id: '062000', nama: 'KAB. PIDIE JAYA' },
+        { id: '066100', nama: 'KOTA BANDA ACEH' },
+        { id: '066000', nama: 'KOTA SABANG' },
+        { id: '066200', nama: 'KOTA LHOKSEUMAWE' },
+        { id: '066300', nama: 'KOTA LANGSA' },
+        { id: '066400', nama: 'KOTA SUBULUSSALAM' },
       ],
     };
 
@@ -84,7 +86,8 @@ describe('Test Regional Data Routes', () => {
   });
 
   it('should return 404 if id is invalid', async () => {
-    const { body, status } = await request(app).get('/api/cities?province_id=100');
+    const token = process.env.API_KEY as string;
+    const { body, status } = await request(app).get('/api/cities?province_id=100').auth(token, { type: 'bearer' });
 
     const expected = {
       message: "Province doesn't exist",
@@ -92,5 +95,12 @@ describe('Test Regional Data Routes', () => {
 
     expect(status).toBe(404);
     expect(JSON.stringify(body)).toMatch(JSON.stringify(expected));
+  });
+
+  it("should return 200 from the school's query", async () => {
+    const token = process.env.API_KEY as string;
+    const { status } = await request(app).get('/api/schools?city_id=316100').auth(token, { type: 'bearer' });
+
+    expect(status).toBe(200);
   });
 });
