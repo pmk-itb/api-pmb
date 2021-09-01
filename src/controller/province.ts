4b175da47602718f6b60717c5e0532f275a95216
Samuel <samnap11@gmail.com>
2021-09-01 12:19:42 +0700

    feat: add error message

--- src/controller/province.ts
+++ src/controller/province.ts
@@ -6,7 +6,7 @@ const listProvinces = async (_: Request, res: Response): Promise<void> => {
     const data = await getProvincesData();
     res.status(200).json({ data });
   } catch (error) {
-    res.status(500);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
