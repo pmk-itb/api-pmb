0a3e5cf65aa8ec7cc11bdca85827c3208c3d11c8
Samuel <samnap11@gmail.com>
2021-09-02 17:35:46 +0700

    fix: move CORS to load before everything else

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import cors from 'cors';
 const app = express();
 
 // To parse json from the request
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
-app.use(cors());
 
 export = app;
